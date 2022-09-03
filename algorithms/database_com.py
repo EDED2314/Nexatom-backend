dc38ea1adf7b55f685af7d9402399cac2163becd
Eddie Tang <eddietang2314@gmail.com>
2022-09-03 15:46:26 -0400

    api

--- algorithms/database_com.py
+++ algorithms/database_com.py
@@ -17,37 +17,41 @@ async def updateUser(atom: Atom, _id:str):
     
     db = client.test
     col = db.get_collection("users")
-    col.update_one({"_id": ObjectId(f'{_id}')},{"$set":{"proccessed":processedArr}})
+    col.update_one({"_id": ObjectId(f'{_id}')},{"$set":{"processed":processedArr}})
     print("updated")
 
 async def findRec(_id:str):
-    await asyncio.sleep(1)
     db = client.test
     col = db.get_collection("users")
     recIds = []
     diffs = []
     results = col.find({"_id": ObjectId(f'{_id}')})
     for result in results:
-        theUserArry = np.array(result.get("proccessed"))
+        theUserArry = np.array(result.get("processed"))
         break
     for doc in col.find():
-        atomArr1 = np.array(doc.get("proccessed"))
+        atomArr1 = np.array(doc.get("processed"))
         if doc.get("_id") != ObjectId(f'{_id}'):
             diffs.append({doc.get("_id"):np.linalg.norm(theUserArry - atomArr1)})
     
     for i in range(3):
-        smallId = ObjectId('63137875afd8bc0b8143642e')
+        smallId = None
         smallestDiff = -1
         for diff in diffs:
-            for _id, diff in diff.items():
-                if smallestDiff == -1 and smallId == ObjectId('63137875afd8bc0b8143642e'):
-                    smallestDiff = diff
+            for _id, difff in diff.items():
+                if smallestDiff == -1 and smallId == None:
+                    smallestDiff = difff
                     smallId = _id
-                elif smallestDiff > diff:
-                    smallestDiff = diff
+                elif smallestDiff > difff:
+                    smallestDiff = difff
                     smallId = _id
-            recIds.append(smallId)
-            del diff[smallId]
+        if not smallId is None:
+            recIds.append(str(smallId))
+            diffs.remove({smallId:smallestDiff})
+            smallId = None
+            smallestDiff = -1
+        else:
+            continue
                 
 
     return recIds
